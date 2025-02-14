9adb782008b16f74297904fc14dbbb25cc3ee67d
Abhinay Balusu <abalusu@paypal.com>
2025-02-14 11:39:02 -0700

    updated comments

--- Sources/BraintreePayPal/BTPayPalClientAppLaunchConfig.swift
+++ Sources/BraintreePayPal/BTPayPalClientAppLaunchConfig.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/// Used for switching to either PayPal app or open fallback url in a browser authentication session
+/// Used for switching to either PayPal app or open fallback url in external browser
 struct BTPayPalClientAppLaunchConfig {
 
     // The universal link URL used to launch the PayPal client app.
@@ -16,7 +16,7 @@ struct BTPayPalClientAppLaunchConfig {
     // The BA (Billing Agreement) token
     let baToken: String
 
-    // An optional fallback URL to be used if the universal link fails.
+    // An optional fallback URL used to open in a external browser if the universal link fails.
     let fallbackUrl: URL?
 
     init(appSwitchUrl: URL, baToken: String, fallbackUrl: URL? = nil) {
